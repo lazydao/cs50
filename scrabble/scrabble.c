ea3e7cd4fcb16334facb2d40b803b967bde170cb
Will Liu <daodao1990@gmail.com>
2024-07-04 15:46:09 +0800

    Update scrabble.c

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,25 +1,44 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
+#include <string.h>
 
-// Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
-int compute_score(string word);
+int get_score(string word)
+{
+    int score = 0;
+    for (int i = 0, n = strlen(word); i < n; i++)
+    {
+        if (word[i] >= 'A' && word[i] <= 'z')
+        {
+            int index = toupper(word[i]) - 'A';
+            score += POINTS[index];
+        }
+    }
+    return score;
+}
 
 int main(void)
 {
-    // Get input words from both players
+    // Prompt the user for two words
     string word1 = get_string("Player 1: ");
     string word2 = get_string("Player 2: ");
-
-    // Score both words
-    int score1 = compute_score(word1);
-    int score2 = compute_score(word2);
-
-    // TODO: Print the winner
-}
-
-int compute_score(string word)
-{
-    // TODO: Compute and return score for string
+    // Compute the score of each word
+    int score1 = get_score(word1);
+    int score2 = get_score(word2);
+    // Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!");
+    }
+    else if (score1 < score2)
+    {
+        printf("Player 2 wins!");
+    }
+    else
+    {
+        printf("Tie!");
+    }
+    printf("\n");
 }
